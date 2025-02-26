5cbfeb4fd4ae6d487945eceac920537cd6d31e59
vishal shahi <vishal.shahi.ece23@itbhu.ac.in>
2025-02-26 15:56:07 +0530

    Added Landing and signup page

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-
-from flask import Flask, send_file
+from flask import Flask, send_file, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,8 +7,15 @@ app = Flask(__name__)
 def index():
     return send_file('src/index.html')
 
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+@app.route("/signup")
+def signup():
+    return send_file('src/signup.html')
+
+@app.route("/get-started")
+def get_started():
+    # Redirect to the signup page
+    return redirect(url_for('signup'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Using port 5000 to avoid permission issues
+    app.run(debug=True, port=port)
